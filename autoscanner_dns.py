3bb71321066128ba16548b2cd114457ea36e996b
sapage <sapage@sapage.net>
2022-01-05 09:10:59 +1000

    Update autoscanner_dns.py

--- autoscanner_dns.py
+++ autoscanner_dns.py
@@ -4,24 +4,22 @@ from netbox import NetBox
 import requests
 import datetime
 import json
-
-from dns import resolver
-from dns import reversename
+import socket
 
 requests.packages.urllib3.disable_warnings()
 
 
-API_TOKEN = "xx"
+API_TOKEN = "d3a51fe6e4ab6419c731ab0de11335c3682bcd06"
 HEADERS = {'Authorization': f'Token {API_TOKEN}', 'Content-Type': 'application/json', 'Accept': 'application/json'}
-NB_URL = "https://x.x.x.x"
-netbox = NetBox(host="x.x.x.x", port=443, use_ssl=True, auth_token="xx")
+NB_URL = "https://10.53.109.145"
+netbox = NetBox(host="10.53.109.145", port=443, use_ssl=True, auth_token="d3a51fe6e4ab6419c731ab0de11335c3682bcd06")
 
 if __name__ == '__main__':
 
     # Define the network to scan
     #my_network = input("Subnet: ")    
 
-    network = ["10.53.109.1/32"]
+    network = ["10.53.109.53/32"]
     for my_network in network:
             
         # Create the object
@@ -41,11 +39,7 @@ if __name__ == '__main__':
             request_url = f"{NB_URL}/api/ipam/ip-addresses/?q={ipaddress}/"
             ipaddress1 = requests.get(request_url, headers = HEADERS, verify=False)
             netboxip = ipaddress1.json()
-            pretty_obj = json.dumps(netboxip, indent=4)
-            ipam_ip_dict = json.loads(pretty_obj)
-            ipam_ip_url=(ipam_ip_dict['results'][0]['url'])
-            
-           
+           # print(pretty_obj)
             if netboxip['count'] == 0:
                 # Check if in network exists and not exist in netbox
                 if ipaddress in found_ip_in_network:
@@ -56,9 +50,13 @@ if __name__ == '__main__':
             else:
                 #If not exists in netbox and network
                 if ipaddress in found_ip_in_network:
-                    print(ipaddress)
-                    addr = reversename.from_address(ipaddress)
-                    jsonUpdate = '{"vrf": 1, "tenant": 1, "dns_name": addr, "status": "active"}'
+                    addr = socket.gethostbyaddr(str(ipaddress))
+                    pretty_obj = json.dumps(netboxip, indent=4)
+                    ipam_ip_dict = json.loads(pretty_obj)
+                    ipam_ip_url=(ipam_ip_dict['results'][0]['url'])
+                    jsonUpdate_temp = '{"vrf": 1, "tenant": 1, "dns_name": "replace", "status": "active"}'
+                    jsonUpdate = jsonUpdate_temp.replace('replace', addr[0],1)
                     response = requests.patch(ipam_ip_url, data=(jsonUpdate), headers=HEADERS, verify=False)
                 else:
                     netbox.ipam.update_ip(str(ipaddress),status="deprecated")
+#
