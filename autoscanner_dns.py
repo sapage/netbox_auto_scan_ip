2a68876cf96ce2f6e3549098d0d232e3ba866f34
sapage <sapage@sapage.net>
2022-01-05 13:42:08 +1000

    Update autoscanner_dns.py

--- autoscanner_dns.py
+++ autoscanner_dns.py
@@ -15,17 +15,17 @@ def dns_lookup(host):
 
 requests.packages.urllib3.disable_warnings()
 
-API_TOKEN = "d3a51fe6e4ab6419c731ab0de11335c3682bcd06"
+API_TOKEN = "xx"
 HEADERS = {'Authorization': f'Token {API_TOKEN}', 'Content-Type': 'application/json', 'Accept': 'application/json'}
 NB_URL = "https://10.53.109.145"
-netbox = NetBox(host="10.53.109.145", port=443, use_ssl=True, auth_token="d3a51fe6e4ab6419c731ab0de11335c3682bcd06")
+netbox = NetBox(host="10.53.109.145", port=443, use_ssl=True, auth_token="xx")
 
 if __name__ == '__main__':
 
     # Define the network to scan
     #my_network = input("Subnet: ")    
 
-    network = ["10.53.109.0/24"]
+    network = ["10.53.4.0/24", "10.1.0.0/24", "10.53.102.0/24", "10.53.103.0/24", "10.53.110.0/24", "10.53.112.0/24", "10.1.15.0/24", "10.1.22.0/24", "10.1.27.0/24", "10.1.29.0/24", "10.1.0.0/24", "10.1.4.0/24", "10.1.5.0/24", "10.1.55.0/24", "10.51.2.0", "10.1.14.0/24", "10.1.21.0/24", "10.2.0.0/24", "10.53.2.0/24", "10.51.253.0/24", "10.2.55.0/24", "10.51.200.0/24", "10.51.201.0/24", "10.51.206.0/24", "10.53.105.0/24", "10.53.98.0/24", "10.53.100.0/24", "10.53.109.0/24", "10.53.115.0/24", "10.53.117.0/24", "10.53.119.0/24", "10.53.124.0/24", "10.53.174.0/24", "10.53.128.0/24", "10.53.101.0/24", "10.53.16.0/24" , "10.53.122.0/24", "10.53.126.0/24", "10.53.176.0/24", "10.153.101.0/24", "10.153.102.0/24", "10.153.103.0/24", "10.153.104.0/24", "10.153.105.0/24", "10.153.107.0/24", "10.153.108.0/24", "10.153.109.0/24", "10.153.119.0/24", "10.153.120.0/24", "10.153.96.0/24", "10.153.97.0/24", "10.153.98.0/24", "10.153.99.0/24", "10.153.126.0/24", "10.153.127.0/24", "10.153.128.0/24", "10.153.129.0/24", "10.153.100.0/24", "10.53.1.0/24", "10.53.24.0/24", "10.58.1.0/24", "10.53.241.0/24", "10.53.26.0/24"] 
     for my_network in network:
             
         # Create the object
@@ -75,16 +75,13 @@ if __name__ == '__main__':
                     else:
                         netbox.ipam.update_ip(str(ipaddress),status="deprecated")
             elif ipaddress in found_ip_in_network:
-                addr = dns_lookup(str(ipaddress))
-                for answer in addr:
-                    answer.to_text()
-                    print(answer)
-                    pretty_obj = json.dumps(netboxip, indent=4)
-                    ipam_ip_dict = json.loads(pretty_obj)
-                    ipam_ip_url=(ipam_ip_dict['results'][0]['url'])
-                    jsonUpdate_temp = '{"vrf": 1, "tenant": 1, "dns_name": "replace", "status": "active"}'
-                    jsonUpdate = jsonUpdate_temp.replace('replace', str(answer))
-                    #jsonUpdate = '{"vrf": 1, "tenant": 1, "dns_name": "replace", "status": "active"}'
-                    response = requests.create(ipam_ip_url, data=(jsonUpdate), headers=HEADERS, verify=False)
+                print(str(ipaddress))
+                netbox.ipam.create_ip_address(str(ipaddress))
+                #jsonUpdate_temp = '{"vrf": 1, "tenant": 1, "dns_name": "replace", "status": "active"}'
+                #jsonUpdate = jsonUpdate_temp.replace('replace', str(answer))
+                #jsonUpdate = '{"vrf": 1, "tenant": 1, "dns_name": "New IP", "status": "active", "address": "ipadd"}'
+                #jsonUpdate = jsonUpdate_temp.replace('ipadd', str(ipaddress))
+                #request_url_post = f"{NB_URL}/api/ipam/ip-addresses/"
+                #response = requests.post(request_url_post, data=(jsonUpdate), headers=HEADERS, verify=False)
             else:
                 print('NO IP Address')
